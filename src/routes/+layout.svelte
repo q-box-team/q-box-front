ff6c69386121e7cd2983c973bebb80083b199645
phjppo0918 <phjppo0918@gmail.com>
2023-10-25 12:40:40 +0900

    fix: api url 환경변수로 입력할 수 있도록 설정

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,8 @@
 <script>
     import Header from "./Header.svelte";
     import axios from "axios";
-
-    //    axios.defaults.baseURL = "https://q-box.site/api";
-    axios.defaults.baseURL = "http://localhost:8080/api";
+    
+    axios.defaults.baseURL = import.meta.env.VITE_API_URL;
     axios.defaults.withCredentials = true;
 </script>
 
