537e926197440a76a53f9ecbbdee44799c20523a
SANGYOON PARK <parkyoon97@gmail.com>
2023-11-14 03:02:25 +0900

    chore: 불필요한 코드 제거

--- src/routes/(mono-column)/+layout.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-    import axios from "axios";
-    import Header from "./Header.svelte";
-    axios.defaults.baseURL = import.meta.env.VITE_API_URL;
-    axios.defaults.withCredentials = true;
-</script>
-<Header/>
-
-<slot></slot>
