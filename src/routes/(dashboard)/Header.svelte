ea524377bb3d9d354f022a5f910e913298ab76a8
SANGYOON PARK <parkyoon97@gmail.com>
2023-11-27 22:59:42 +0900

    design: Dashboard Header

--- src/routes/(dashboard)/Header.svelte
+++ src/routes/(dashboard)/Header.svelte
@@ -1,10 +1,58 @@
 <script>
-
-
-    let topAppBar;
+  import Button, { Label } from "@smui/button";
+  import IconButton, { Icon } from '@smui/icon-button';
+  import { mdiAccountCircle } from '@mdi/js';
 </script>
 
-
-<div style="width: 100%; background-color: #6c76c5; height: 60px;">
-    <div>dd</div>
+<div
+  class="hg_70 flex-child_j-between_a-start bg-c_sf"
+  style="width: 100%; padding-left: 20px;"
+>
+  <div style="flex: 0.735">
+    <Button
+      color="surface"
+      class="wd_200 bd-rd_28 hg_50"
+      style="margin-top: 1vh;"
+      variant="raised"
+    >
+      <Label class="fnt-sz_20 fnt-wg_600">컴퓨터전공</Label>
+    </Button>
+  </div>
+  <div class="flex-child_j-between_a-center" style="flex: 0.265">
+    <div>
+        <Button
+          color="surface"
+          class="wd_200 bd-rd_28 hg_50"
+          style="margin-top: 1vh;"
+          variant="raised"
+        >
+          <Label class="fnt-sz_20 fnt-wg_600">질문하기</Label>
+        </Button>
+    </div>
+    <div>
+      <div class="wd_55 bg-c_pri" style="margin-right: 40px; margin-top: 6px; border-radius: 20px;">
+        <IconButton style="width: 55px; height: 55px;">
+            <Icon tag="svg" viewBox="0 0 23 24">
+                <path fill="#f0f2f7" d={mdiAccountCircle}/>
+            </Icon>
+        </IconButton>
+      </div>
+    </div>
+  </div>
 </div>
+
+<style>
+  *
+    :global(
+      .shaped-outlined .mdc-notched-outline .mdc-notched-outline__leading
+    ) {
+    border-radius: 28px 0 0 28px;
+    width: 28px;
+  }
+  *
+    :global(
+      .shaped-outlined .mdc-notched-outline .mdc-notched-outline__trailing
+    ) {
+    border-radius: 0 28px 28px 0;
+  }
+</style>
