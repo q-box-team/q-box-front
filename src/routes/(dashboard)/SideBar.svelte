a112d1849b7228511701993b358066f2d3511dcf
SANGYOON PARK <parkyoon97@gmail.com>
2023-11-28 20:54:34 +0900

    style: sidebar hover effect update

--- src/routes/(dashboard)/SideBar.svelte
+++ src/routes/(dashboard)/SideBar.svelte
@@ -10,14 +10,27 @@
   let ifNotCreated = [];
 </script>
 
-<div class="flex-child_j-start_a-start f_column bd-1_black bg-c_pri blink_disable" style="height: 100vh; flex: 0.15;">
+<div class="flex-child_j-start_a-start f_column bg-c_pri blink_disable" style="height: 100vh; flex: 0.15;">
   <div class="hg_70 wd_100p flex-child_center" ><img src="/favicon.png" alt="이미지" style="width: 50px; height: 50px;"/></div>
   <div class="wd_100p">
     {#each createdDeparts as depart}
-      <div class="hg_60 flex-child_j-start_a-center bd-1_black c_wt fnt-sz_18" style="padding-left: 30px;"><span>{depart}</span></div>
+      <div class="subject hg_65 flex-child_j-start_a-center c_wt fnt-sz_20" style="padding-left: 30px;"><span><b>{depart}</b></span></div>
     {/each}
-    <div class="hg_60 flex-child_center bd-1_black" on:click={() => {alert("과목을 추가하시겠습니까?")}} role="button">
-      <img src="/plusicon.png" class="wd_30 hg_30" alt="이미지"/>
+    <div class="subject-add_button hg_65 flex-child_center" role="button">
+      <div class="wd_40 hg_40 bd-rd_50 bg-c_sf flex-child_center">
+        <div class="material-icons wd_30 hg_30" style="position: relative; top: 3px; left: 3px; color: #6A76Cb;">add</div>
+      </div>
     </div>
   </div>
 </div>
+
+<style>
+  .subject:hover {
+    background-color: #f0f2f7;
+    color: #6A76CB;
+    cursor: pointer;
+  }
+  .subject-add_button:hover {
+    background-color: #f0f2f7;
+  }
+</style>
