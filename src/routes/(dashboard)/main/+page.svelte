81a35f882ef36ec5e64809ff57a4106d7930a2e3
SANGYOON PARK <parkyoon97@gmail.com>
2023-11-28 20:18:58 +0900

    feat: 평가 분포 그래프 UI

--- src/routes/(dashboard)/main/+page.svelte
+++ src/routes/(dashboard)/main/+page.svelte
@@ -1,7 +1,7 @@
 <script>
     import QuestionList from "../common/QuestionList.svelte";
     import Subject from "../common/Subject.svelte";
-    import Calendar from "../common/Calendar.svelte";
+    import RadarGraph from "../common/RadarGraph.svelte";
     import TopPerformer from "../common/TopPerformer.svelte";
 </script>
 
@@ -11,7 +11,7 @@
         <div class="flex-child_center wd_100p" style="flex: 0.75"><QuestionList/></div>
     </div>
     <div class="flex-child_j-start_a-center f_column" style="flex: 0.24; height: calc(100vh - 60px); gap: 20px;">
-        <div class="wd_100p" style="flex: 0.45;"><Calendar/></div>
+        <div class="wd_100p" style="flex: 0.45;"><RadarGraph/></div>
         <div class="wd_100p" style="flex: 0.50;"><TopPerformer/></div>
     </div>
 </div>
