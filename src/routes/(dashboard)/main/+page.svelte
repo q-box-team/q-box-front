9047b9bd63f089d2d6159aeb9f05f30b7a8ab45e
SANGYOON PARK <parkyoon97@gmail.com>
2023-11-28 17:50:59 +0900

    design: Top Performer Card UI

--- src/routes/(dashboard)/main/+page.svelte
+++ src/routes/(dashboard)/main/+page.svelte
@@ -7,11 +7,11 @@
 
 <div class="flex-child_j-start_a-start f_row" style="width: 100%;">
     <div class="flex-child_j-start_a-start f_column" style="flex: 0.75; height: calc(100vh - 60px);">
-        <div class="flex-child_center wd_100p" style="flex: 0.25"><Subject/></div>
+        <div class="flex-child_j-start_a-start wd_100p" style="flex: 0.25"><Subject/></div>
         <div class="flex-child_center wd_100p" style="flex: 0.75"><QuestionList/></div>
     </div>
-    <div class="flex-child_j-start_a-center f_column" style="flex: 0.24; height: calc(100vh - 60px);">
-        <div class="wd_100p bd-1_black" style="flex: 0.5;"><Calendar/></div>
-        <div class="wd_100p bd-1_black" style="flex: 0.5;"><TopPerformer/></div>
+    <div class="flex-child_j-start_a-center f_column" style="flex: 0.24; height: calc(100vh - 60px); gap: 20px;">
+        <div class="wd_100p" style="flex: 0.45;"><Calendar/></div>
+        <div class="wd_100p" style="flex: 0.50;"><TopPerformer/></div>
     </div>
 </div>
