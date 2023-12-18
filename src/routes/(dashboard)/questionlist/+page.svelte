2c1fd5370bab534186f23039b7c113fcbcef498e
SANGYOON PARK <parkyoon97@gmail.com>
2023-12-18 17:11:54 +0900

    feat: markdown vditor 적용

---
+++ src/routes/(dashboard)/questionlist/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+    import { onMount } from "svelte"
+    import Vditor from "vditor"
+    onMount(() => {
+        const vditor = new Vditor("vditor-container", {
+            theme: "classic",
+            minHeight: 630,
+        })
+    })
+</script>
+    
+<div id="vditor-container" />
+
+<style>
+</style>
