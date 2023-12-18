105ad66b78f25069d0f51a3cf95a2a335a3811e2
SANGYOON PARK <parkyoon97@gmail.com>
2023-12-18 18:07:10 +0900

    style: markdown editor update

--- src/routes/(dashboard)/questionform/+page.svelte
+++ src/routes/(dashboard)/questionform/+page.svelte
@@ -1,49 +1,48 @@
 <script>
-    // Import markdown conversion library
-    import {marked} from 'marked'
+  import { onMount } from "svelte";
+  import Vditor from "vditor";
 
-    // Declare a variable to store the markdown data
-    let markdown = ""
+  onMount(() => {
+    const vditor = new Vditor("vditor-container", {
+      theme: "dark2",
+      minHeight: 630,
+      lang: "ko_KR",
+      mode: "sv",
+      width: 1265,
+      placeholder: "질문 내용을 입력해주세요",
+      isPreview: true,
+      isOpen: true,
+      getValue: () => {
+        console.log('hi');
+      },
+      upload: {
+        // 파일 업로드 성공 시 실행되는 콜백 함수
+        success: (editorElement, responseText) => {
+          // 여기에 성공 시 수행할 동작을 작성
+          return alert("성공");
+        },
+
+        // 파일 업로드 실패 시 실행되는 콜백 함수
+        error: (responseText) => {
+          // 여기에 실패 시 수행할 동작을 작성
+          return alert("실패");
+        },
+
+        // 파일 업로드 응답 데이터 포맷 변환 시 실행되는 콜백 함수
+        format: (files, responseText) => {
+          // 여기에 포맷 변환 시 수행할 동작을 작성
+          // files는 업로드한 파일들의 배열, responseText는 서버 응답 텍스트
+          // 변환된 결과를 반환
+          return transformedResponseText;
+        },
+      },
+    });
+  });
 </script>
 
-<h1>Markdown Editor</h1>
+<div style="padding: 0px 20px">
+  <div id="vditor-container" />
+</div>
 
-<!-- Declare a textarea where the user can enter markdown, and bind it to the variable `markdown` -->
-<textarea bind:value={markdown} placeholder="Enter markdown here"/>
-
-<!-- Convert the markdown to HTML and display it -->
-<div class="preview">{@html marked(markdown)}</div>
-
-<!-- Make it look (slightly) nicer ;) -->
 <style>
-    textarea, .preview {
-        box-sizing: border-box;
-        display: block;
-        width: 100%;
-    }
-
-    textarea {
-        font-family: monospace, Roboto;
-        height: 50%;
-        border: none;
-        margin: 0;
-        padding: 1rem;
-    }
-
-    .preview {
-        height: 50%;
-        padding: 2rem;
-        border-top: solid 2px #888;
-    }
-
-    h1 {
-        background: #e1e1e1;
-        margin: 0px;
-        padding: 0.8rem;
-        font-size: 1.2rem;
-    }
-
-    :global(body) {
-        padding: 0;
-    }
 </style>
