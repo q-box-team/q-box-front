eaeb32fd35e247ac9240942a86b5018431df7118
SANGYOON PARK <parkyoon97@gmail.com>
2023-12-18 17:12:25 +0900

    style: 해상도 조절

--- src/routes/(dashboard)/common/TopPerformer.svelte
+++ src/routes/(dashboard)/common/TopPerformer.svelte
@@ -21,6 +21,11 @@
       nickname: "감성의 나그네",
       userDetail: "ddd@kpu.ac.kr",
     },
+    {
+      img: "https://avatars.githubusercontent.com/u/15474343?v=4",
+      nickname: "별빛의 여인",
+      userDetail: "ccc@kpu.ac.kr",
+    },
   ];
 </script>
 
@@ -28,7 +33,7 @@
   class="bg-c_wt wd_100p hg_100p blink_disable bd-rd_35"
   style="box-shadow: 0px 8px 13px #9d9d9d, 0px 25px #f0f2f7;"
 >
-  <div class="fnt-sz_30" style="padding-top: 30px; padding-left: 30px; padding-bottom: 40px;"><b>TopPerformer</b></div>
+  <div class="fnt-sz_30" style="padding-top: 30px; padding-left: 30px; padding-bottom: 25px;"><b>TopPerformer</b></div>
   <div class="flex-child_j-start_a-start f_column" style="gap: 30px; padding-left: 30px; padding-right: 30px;">
     {#each questions as question}
       <div class="flex-child_j-between_a-center f_row wd_100p">
--- src/routes/(dashboard)/main/+page.svelte
+++ src/routes/(dashboard)/main/+page.svelte
@@ -7,7 +7,7 @@
 
 <div class="flex-child_j-start_a-start f_row" style="width: 100%;">
     <div class="flex-child_j-start_a-start f_column" style="flex: 0.75; height: calc(100vh - 60px);">
-        <div class="flex-child_j-start_a-start wd_100p" style="flex: 0.25"><Subject/></div>
+        <div class="flex-child_j-start_a-start wd_100p" style="flex: 0.25; margin-bottom: 20px"><Subject/></div>
         <div class="flex-child_center wd_100p" style="flex: 0.75"><QuestionList/></div>
     </div>
     <div class="flex-child_j-start_a-center f_column" style="flex: 0.24; height: calc(100vh - 60px); gap: 20px;">
