5368d627999c525b5cbb401838eb41c38c37f0e9
SANGYOON PARK <parkyoon97@gmail.com>
2023-12-19 20:42:04 +0900

    feat: QuestionList presentational & container design pattern apply

---
+++ src/routes/(dashboard)/questionlist/+page.svelte
@@ -0,0 +1,73 @@
+<script>
+    import Card from "./Card.svelte";
+    const questions = [
+      {
+        img: "https://avatars.githubusercontent.com/u/15474343?v=4",
+        nickname: "물결의 무인",
+        detail: "대용 교수님이 무슨 말을 하는지 이해를 못하겠어요",
+        thumbUp: 3,
+      },
+      {
+        img: "https://i.stack.imgur.com/frlIf.png",
+        nickname: "감성의 나그네",
+        detail: "CORS 216번 어떻게 푸나요ㅠㅠㅠ 알려주세요",
+        thumbUp: 2,
+      },
+      {
+        img: "https://avatars.githubusercontent.com/u/15474343?v=4",
+        nickname: "별빛의 여인",
+        detail: "13942번 문제에서 자꾸 StackSizeExceed가 나와요",
+        thumbUp: 1,
+      },
+      {
+        img: "https://i.stack.imgur.com/frlIf.png",
+        nickname: "감성의 나그네",
+        detail: "CORS 216번 어떻게 푸나요ㅠㅠㅠ 알려주세요",
+        thumbUp: 2,
+      },
+      {
+        img: "https://avatars.githubusercontent.com/u/15474343?v=4",
+        nickname: "별빛의 여인",
+        detail: "13942번 문제에서 자꾸 StackSizeExceed가 나와요",
+        thumbUp: 1,
+      },
+      {
+        img: "https://i.stack.imgur.com/frlIf.png",
+        nickname: "감성의 나그네",
+        detail: "CORS 216번 어떻게 푸나요ㅠㅠㅠ 알려주세요",
+        thumbUp: 2,
+      },
+      {
+        img: "https://avatars.githubusercontent.com/u/15474343?v=4",
+        nickname: "별빛의 여인",
+        detail: "13942번 문제에서 자꾸 StackSizeExceed가 나와요",
+        thumbUp: 1,
+      },
+      {
+        img: "https://avatars.githubusercontent.com/u/15474343?v=4",
+        nickname: "별빛의 여인",
+        detail: "13942번 문제에서 자꾸 StackSizeExceed가 나와요",
+        thumbUp: 1,
+      },
+      {
+        img: "https://i.stack.imgur.com/frlIf.png",
+        nickname: "감성의 나그네",
+        detail: "CORS 216번 어떻게 푸나요ㅠㅠㅠ 알려주세요",
+        thumbUp: 2,
+      },
+      {
+        img: "https://avatars.githubusercontent.com/u/15474343?v=4",
+        nickname: "별빛의 여인",
+        detail: "13942번 문제에서 자꾸 StackSizeExceed가 나와요",
+        thumbUp: 1,
+      },
+      {
+        img: "https://avatars.githubusercontent.com/u/15474343?v=4",
+        nickname: "별빛의 여인",
+        detail: "13942번 문제에서 자꾸 StackSizeExceed가 나와요",
+        thumbUp: 1,
+      },
+    ];
+  </script>
+
+<Card questions={questions}/>
