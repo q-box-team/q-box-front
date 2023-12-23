22b9a02a7b7bfbc0661f77ad37a0239138778073
SANGYOON PARK <parkyoon97@gmail.com>
2023-12-24 02:12:10 +0900

    fix: http header

--- src/routes/(mono-column)/signup/+page.svelte
+++ src/routes/(mono-column)/signup/+page.svelte
@@ -21,24 +21,25 @@
     }
     const centerPosition = (slider.clientWidth * currentSlidePosition) / 600;
     slider.scrollTo({ left: centerPosition, behavior: "smooth" });
-  }
+  };
 
   const emailAuth = async (email) => {
     userEmail = email;
     const requestData = {
       email: email,
     };
+    const fetchData = {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json;charset=UTF-8'
+      },
+      body: JSON.stringify(requestData)
+    }
     if (validateEmail(email)) {
       alert("이메일 형식이 올바릅니다.");
-      await fetch("https://dev.q-box.site/api/emails", {
-        method: "post",
-        body: JSON.stringify(requestData),
-      }).then(async (response) => {
+      await fetch("https://dev.q-box.site/api/emails", fetchData).then(async (response) => {
         if (Response.status >= 200 && Response.status < 300) {
-
-
-
-          slide("next")
+          slide("next");
           return response.json();
         } else {
           const errData = await response.json();
@@ -55,21 +56,26 @@
     const requestData = {
       key: code,
     };
-    await fetch( import.meta.env.VITE_API_URL + `/emails`, {
-        method: "post",
-        body: JSON.stringify(requestData),
-      }).then(async (response) => {
-        if (Response.status >= 200 && Response.status < 300) {
-          console.log(`인증완료`);
-          slide("next")
-          return response.json();
-        } else {
-          const errData = await response.json();
-          console.log(errData);
-          throw new Error("Something went wrong (server side)");
-        }
-      });
-    };
+    const fetchData = {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json;charset=UTF-8'
+      },
+      body: JSON.stringify(requestData)
+    }
+
+    await fetch(import.meta.env.VITE_API_URL + `/emails`, fetchData).then(async (response) => {
+      if (Response.status >= 200 && Response.status < 300) {
+        console.log(`인증완료`);
+        slide("next");
+        return response.json();
+      } else {
+        const errData = response.json();
+        console.log(errData);
+        throw new Error("Something went wrong (server side)");
+      }
+    });
+  };
 
   // 이메일 체크
   const validateEmail = (email) => {
@@ -89,10 +95,9 @@
         {#if step === "emailAuth"}
           <Card {step} {slide} {emailAuth} />
         {:else if step === "authComplete"}
-          <Card {step} {slide} {authCodeVerification} {userEmail}/>
+          <Card {step} {slide} {authCodeVerification} {userEmail} />
         {:else}
           <Card {step} {slide} />
-
         {/if}
       {/each}
     </div>
