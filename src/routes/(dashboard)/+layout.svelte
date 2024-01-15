eb3e87317d93b88ca1ef03871b82349469ff7520
SANGYOON PARK <parkyoon97@gmail.com>
2024-01-15 15:32:52 +0900

    feat: exception before rendering

--- src/routes/(dashboard)/+layout.svelte
+++ src/routes/(dashboard)/+layout.svelte
@@ -1,18 +1,65 @@
 <script>
-    import SubjectModal from "./common/SubjectModal.svelte";
-    import axios from "axios";
-    import SideBar from "./SideBar.svelte";
-    import Header from "./Header.svelte";
-    import "../../global.css";
-    axios.defaults.baseURL = import.meta.env.VITE_API_URL;
-    axios.defaults.withCredentials = true;
-</script>
-<div class="flex-child_j-start_a-start f_row" style="overflow:hidden; width: 100vw; height: 100vh;">
-    <SideBar/>
-    <div class="flex-child_j-start_a-start f_column" style="width: 100%; flex: 0.85">
-        <Header/>
-        <slot></slot>
-    </div>
-    <SubjectModal />
+    import userInfo from '../../store/user-store';
+  import { onMount } from "svelte";
+  import { goto } from "$app/navigation";
+  import SubjectModal from "./common/SubjectModal.svelte";
+  import axios from "axios";
+  import SideBar from "./SideBar.svelte";
+  import Header from "./Header.svelte";
+  import "../../global.css";
+  axios.defaults.baseURL = import.meta.env.VITE_API_URL;
+  axios.defaults.withCredentials = true;
 
+  let user;
+  userInfo.subscribe(info => {
+    // user = info;
+    console.log(info);
+  });
+
+  let cookieString;
+  let jSessionIDIndex = -1;
+
+  onMount(() => {
+    cookieString = document.cookie;
+    jSessionIDIndex = cookieString.indexOf("JSESSIONID=");
+    if (jSessionIDIndex !== -1) {
+      axios
+        .get("/members/me")
+        .then((res) => {
+            userInfo.set({
+                user: {
+                    email: res.data.email,
+                    nickname: res.data.nickname,
+                    depart: {
+                        id: res.data.depart.id,
+                        name: res.data.depart.name,
+                        univId: res.data.depart.univId,
+                    }
+                }
+            });
+        })
+        .catch((e) => {
+          alert("잘못된 접근입니다. 다시 로그인 해주세요.");
+          goto("/login");
+        });
+    } else {
+      alert("잘못된 접근입니다. 다시 로그인 해주세요.");
+      goto("/login");
+    }
+  });
+</script>
+
+<div
+  class="flex-child_j-start_a-start f_row"
+  style="overflow:hidden; width: 100vw; height: 100vh;"
+>
+  <SideBar />
+  <div
+    class="flex-child_j-start_a-start f_column"
+    style="width: 100%; flex: 0.85"
+  >
+    <Header />
+    <slot />
+  </div>
+  <SubjectModal />
 </div>
--- src/routes/(dashboard)/main/+page.svelte
+++ src/routes/(dashboard)/main/+page.svelte
@@ -1,17 +1,30 @@
 <script>
-    import QuestionList from "../common/QuestionList.svelte";
-    import Subject from "../common/Subject.svelte";
-    import RadarGraph from "../common/RadarGraph.svelte";
-    import TopPerformer from "../common/TopPerformer.svelte";
+  import QuestionList from "../common/QuestionList.svelte";
+  import Subject from "../common/Subject.svelte";
+  import RadarGraph from "../common/RadarGraph.svelte";
+  import TopPerformer from "../common/TopPerformer.svelte";
 </script>
 
 <div class="flex-child_j-start_a-start f_row" style="width: 100%;">
-    <div class="flex-child_j-start_a-start f_column" style="flex: 0.75; height: calc(100vh - 60px);">
-        <div class="flex-child_j-start_a-start wd_100p" style="flex: 0.25; margin-bottom: 20px"><Subject/></div>
-        <div class="flex-child_center wd_100p" style="flex: 0.75"><QuestionList/></div>
+  <div
+    class="flex-child_j-start_a-start f_column"
+    style="flex: 0.75; height: calc(100vh - 60px);"
+  >
+    <div
+      class="flex-child_j-start_a-start wd_100p"
+      style="flex: 0.25; margin-bottom: 20px"
+    >
+      <Subject />
     </div>
-    <div class="flex-child_j-start_a-center f_column" style="flex: 0.24; height: calc(100vh - 60px); gap: 20px;">
-        <div class="wd_100p" style="flex: 0.45;"><RadarGraph/></div>
-        <div class="wd_100p" style="flex: 0.50;"><TopPerformer/></div>
+    <div class="flex-child_center wd_100p" style="flex: 0.75">
+      <QuestionList />
     </div>
+  </div>
+  <div
+    class="flex-child_j-start_a-center f_column"
+    style="flex: 0.24; height: calc(100vh - 60px); gap: 20px;"
+  >
+    <div class="wd_100p" style="flex: 0.45;"><RadarGraph /></div>
+    <div class="wd_100p" style="flex: 0.50;"><TopPerformer /></div>
+  </div>
 </div>
